951b326be9a8e6bf3a74c6ab6df4666e57a9e985
Ahmed Amine Doudech <ahmedaminedoudech@gmail.com>
2023-12-16 20:33:51 +0100

    icon setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Icons from "./components/Icons";
 
 function App() {
   return (
     <div className="text-center flex flex-col items-center pt-10">
-      <h1 className="text-blue-500 text-2xl font-bold">Hello Lego!</h1>
+      <h1 className="text-blue-500 text-2xl font-bold">Welcome to legoland!</h1>
       <img src="logo.svg" className="w-14 h-14" />
+      <p className="text-blue-500 text-xl font-bold mt-5">Let's get to work!</p>
     </div>
   );
 }
---
+++ src/components/Icons.jsx
@@ -0,0 +1,7 @@
+import { Menu, Mail, Key } from "lucide-react";
+
+export default {
+  menu: Menu,
+  email: Mail,
+  password: Key,
+};
